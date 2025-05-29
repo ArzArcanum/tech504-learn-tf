607240b63dc0b32ceb3c39cf833324baf98cf611
ArzArcanum <samthacker1999@gmail.com>
2025-05-29 22:10:14 +0100

    Pushing work so I can access it on laptop

--- tf-create-basic-ec2/main.tf
+++ tf-create-basic-ec2/main.tf
@@ -11,16 +11,14 @@ terraform {
 
 # Where to create - set cloud provider
 provider "aws" {
-  region = var.my_region
 }
 # Get the default VPC
 data "aws_vpc" "default"{
   id = "vpc-07e47e9d90d2076da"
 }
 
-# Create a security group resource
 resource "aws_security_group" "app_security_group" {
-  name        = "tech504-sam-tf-allow-port-22-3000-80"
+  name        = "tech504-sam-tf-app-sg"
   description = "Allow SSH from my IP and port 80(HTTP)&3000 from all"
   vpc_id      = data.aws_vpc.default.id
 
@@ -30,7 +28,7 @@ resource "aws_security_group" "app_security_group" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = [var.my_ip_cidr]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   # Allow HTTP (port 80) from all
@@ -51,18 +49,54 @@ resource "aws_security_group" "app_security_group" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+    # Allow egress from all
+  egress {
+    description = "Egress on all ports to anywhere"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   tags = {
     Name = "app_security_group"
   }
 }
 
-resource "aws_instance" "app_instance" {
-  # Which AMI ID to use. ami-0c1c30571d2dae5c9 (for Ubuntu 22.04 LTS)
-  ami = var.app_ami_id
+resource "aws_security_group" "db_security_group" {
+  name        = "tech504-sam-tf-db-sg"
+  description = "Allow inbound SSH and MongoDB access from app-instances, and outbound my IP and port 80(HTTP)&3000 from all"
+  vpc_id      = data.aws_vpc.default.id
 
-  # Which instance type to use
-  instance_type = var.my_instance_type
-  
+  # Allow port 3000 from all
+  ingress {
+    description = "Port 3000 from all"
+    from_port   = 27017
+    to_port     = 27017
+    protocol    = "tcp"
+    security_groups = [aws_security_group.app_security_group.id]
+  }
+  # Allow egress from all
+  egress {
+    description = "Egress on all ports to anywhere"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "db_security_group"
+  }
+
+resource "aws_instance" "app_instance" {
+
+  # Which OS to use
+  ami = "ami-0c1c30571d2dae5c9" # Ubuntu 22.04 LTS
+
+  instance_type = "t3.micro"
+
+  key_name = "tech504-sam-aws-key"
   # Add a public IP to this instance
   associate_public_ip_address = true
 
@@ -71,11 +105,30 @@ resource "aws_instance" "app_instance" {
 
   tags = {
     # Name of the instance
-    Name = var.my_name,
-    Environment = "test"
+    Name = "tech504-sam-app-node",
+    # This is an ansible target node
+    Ansible = "target"
   }
 }
 
+resource "aws_instance" "db_instance" {
 
+  # Which OS to use
+  ami = "ami-0c1c30571d2dae5c9" # Ubuntu 22.04 LTS
 
-# Syntax to HCL is {key = value}
+  instance_type = "t3.micro"
+
+  key_name = "tech504-sam-aws-key"
+  # Add a public IP to this instance
+  associate_public_ip_address = true
+
+  # Add the security group
+  vpc_security_group_ids = [aws_security_group.app_security_group.id]
+
+  tags = {
+    # Name of the instance
+    Name = "tech504-sam-db-node",
+    # This is an ansible target node
+    Ansible = "target"
+  }
+}
